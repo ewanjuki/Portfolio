7230062d55d9979ed7366e341bbfa541c57e7051
Edwin Wanjuki <edwinwanjuki@gmail.com>
2022-01-18 14:17:44 +0300

    add ripple effect to main button

--- script.js
+++ script.js
@@ -58,3 +58,23 @@ document.body.addEventListener("mouseleave", () => {
   mouseCircle.style.opacity = "0";
   mouseDot.style.opacity = "0";
 });
+
+// Main Button
+const mainBtn = document.querySelector(".main-btn");
+
+let ripple;
+mainBtn.addEventListener("mouseenter", (e) => {
+  let left = e.clientX - e.target.getBoundingClientRect().left;
+  let top = e.clientY - e.target.getBoundingClientRect().top;
+
+  ripple = document.createElement("div");
+  ripple.classList.add("ripple");
+  ripple.style.left = `${left}px`;
+  ripple.style.top = `${top}px`;
+  mainBtn.prepend(ripple);
+});
+
+mainBtn.addEventListener("mouseleave", () => {
+  mainBtn.removeChild(ripple);
+});
+// End of Main Button
