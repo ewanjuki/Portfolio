00f1020137d9e201127da52f94e79cf5e3737527
Edwin Wanjuki <edwinwanjuki@gmail.com>
2022-01-20 15:31:20 +0300

    mouse circle transform effect

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const mouseCircle = document.querySelector(".mouse-circle");
 const mouseDot = document.querySelector(".mouse-dot");
 
+let mouseCircleBool = true;
 const mouseCircleFn = (x, y) => {
-  mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
+  mouseCircleBool &&
+    (mouseCircle.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`);
   mouseDot.style.cssText = `top: ${y}px; left: ${x}px; opacity: 1`;
 };
 // End of Mouse Circle
@@ -74,6 +76,38 @@ const stickyElement = (x, y, hoveredEl) => {
 };
 // End of Sticky Element
 
+// Mouse Circle Transform
+const mouseCircleTransform = (hoveredEl) => {
+  if (hoveredEl.classList.contains("pointer-enter")) {
+    hoveredEl.onmousemove = () => {
+      mouseCircleBool = false;
+      mouseCircle.style.cssText = `
+        width:${hoveredEl.getBoundingClientRect().width}px;
+        height:${hoveredEl.getBoundingClientRect().height}px;
+        top:${hoveredEl.getBoundingClientRect().top}px;
+        left:${hoveredEl.getBoundingClientRect().left}px;
+        opacity: 1;
+        transform: translate(0,0);
+        animation: none;
+        border-radius:${getComputedStyle(hoveredEl).borderBottomLeftRadius};
+        transition: width .5s, height .5s, top .5s, left .5s, 
+          transform .5s, border-radius .5s;
+      `;
+    };
+
+    hoveredEl.onmouseleave = () => {
+      mouseCircleBool = true;
+    };
+
+    document.onscroll = () => {
+      if (!mouseCircleBool) {
+        mouseCircle.style.top = `${hoveredEl.getBoundingClientRect().top}px`;
+      }
+    };
+  }
+};
+// End of Mouse Circle Transform
+
 document.body.addEventListener("mousemove", (e) => {
   let x = e.clientX;
   let y = e.clientY;
@@ -84,6 +118,8 @@ document.body.addEventListener("mousemove", (e) => {
   const hoveredEl = document.elementFromPoint(x, y);
 
   stickyElement(x, y, hoveredEl);
+
+  mouseCircleTransform(hoveredEl);
 });
 
 document.body.addEventListener("mouseleave", () => {
@@ -270,6 +306,8 @@ projects.forEach((project, i) => {
     };
 
     document.removeEventListener("scroll", scrollFn);
+
+    mouseCircle.style.opacity = '0';
 
     progressBarFn(bigImgWrapper);
 
